334d53d32c382cc151647fd73f79eb36e98c4531
Pallavi <kedarpallavi723@gmail.com>
2026-05-21 12:19:35 +0530

    changes done

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -4,14 +4,14 @@ import styles from "./Gallery.module.css";
 const Gallery = () => {
 
   const galleryItems = [
-  {  img: "https://firebasestorage.googleapis.com/v0/b/abhi-9bd8f.firebasestorage.app/o/SARASWATI%20NAGRI%202%20(1).jpg?alt=media&token=8de92aab-be4f-4100-8956-a8655cf299a8" },
-  {  img: "https://firebasestorage.googleapis.com/v0/b/abhi-9bd8f.firebasestorage.app/o/SARASWATI%20NAGRI%203%20(1).jpg?alt=media&token=e4ca4d02-449e-4a9c-ae35-2e171fa54bbb" },
-  {  img: "https://firebasestorage.googleapis.com/v0/b/abhi-9bd8f.firebasestorage.app/o/SATASWATI%20NAGRI%204%20(1).jpg?alt=media&token=4e31ec32-e733-4639-98f7-d66eb7a0a40e" },
-  { img: "https://firebasestorage.googleapis.com/v0/b/abhi-9bd8f.firebasestorage.app/o/SARASWATI%20NAGRI%205%20(1).jpg?alt=media&token=c556b5a4-ffde-4b3f-b257-ec77f63f7fcc"},
-  {  img: "https://firebasestorage.googleapis.com/v0/b/abhi-9bd8f.firebasestorage.app/o/SARASWATI%20NAGRI%206%20(1).jpg?alt=media&token=a8afe780-bc9c-41c2-9514-021caf770f4e" },
-  { img: "https://firebasestorage.googleapis.com/v0/b/abhi-9bd8f.firebasestorage.app/o/8_1%20-%20Photo_21%20-%20Photo%20(1).jpg?alt=media&token=1b6f6b9b-3028-49ff-9e9c-7f2082704fe2" },
-  { img:"https://firebasestorage.googleapis.com/v0/b/abhi-9bd8f.firebasestorage.app/o/saraswati%20nagri%208_2%20-%20Photo%20(1).jpg?alt=media&token=21298f0a-4e8e-41b1-9658-f109f0b32c1f" },
-  // {  img:"https://firebasestorage.googleapis.com/v0/b/abhi-9bd8f.firebasestorage.app/o/SARASWATI%20NAGRI%209%20(1).jpg?alt=media&token=..." },
+  {  img:import.meta.env.VITE_MAP_IMAGE_URL},
+  {  img: import.meta.env.VITE_MAP_IMAGE_URL1 },
+  {  img: import.meta.env.VITE_MAP_IMAGE_URL2 },
+  { img: import.meta.env.VITE_MAP_IMAGE_URL3},
+  {  img: import.meta.env.VITE_MAP_IMAGE_URL4 },
+  { img: import.meta.env.VITE_MAP_IMAGE_URL5 },
+  
+  
 ];
 
   return (
